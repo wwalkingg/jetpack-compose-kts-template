6ca408e96d48f389d768276f1b076b30f4d9da5f
wwalkingg <wwalkingg@outlook.com>
2022-06-21 02:22:41 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,14 @@
+val composeVersion = extra["compose.version"] as String
+
 plugins {
     id("com.android.application")
     kotlin("android")
+    id("org.jetbrains.kotlin.kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
+    namespace = "top.notbe.jetpackComposeKtsTemplate"
     compileSdk = 32
     defaultConfig {
         applicationId = "top.notbe.jetpackComposeKtsTemplate"
@@ -21,7 +26,11 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            //proguardFiles getDefaultProguardFile ('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
         }
     }
     compileOptions {
@@ -35,7 +44,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.1.1"
+        kotlinCompilerExtensionVersion = composeVersion
     }
     packagingOptions {
         resources {
@@ -43,13 +52,13 @@ android {
         }
     }
 }
-val composeVersion = extra["compose.version"] as String
 dependencies {
+    val lifecycleVersion = "2.5.0-rc01"
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.activity:activity-compose:1.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
@@ -57,9 +66,44 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
-
-    val lifecycleVersion = "2.5.0-rc01"
+    // view-model
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycleVersion")
+    // accompanist
+    val accompanistVersion = "0.24.10-beta"
+    implementation("com.google.accompanist:accompanist-pager:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-flowlayout:$accompanistVersion")
+    implementation("com.google.accompanist:accompanist-permissions:$accompanistVersion")
+    // datetime
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
+    // lottie - compose
+    implementation("com.airbnb.android:lottie-compose:5.2.0")
+    // ktor client
+    val ktorVersion = "2.0.2"
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    // navigation
+    implementation("androidx.navigation:navigation-compose:2.4.2")
+    // coil - load image
+    implementation("io.coil-kt:coil-compose:2.1.0")
+    // napier - kotlin log
+    implementation("io.github.aakira:napier:2.6.1")
+    // exposed
+    implementation("org.jetbrains.exposed", "exposed-core", "0.38.1")
+    implementation("org.jetbrains.exposed", "exposed-dao", "0.38.1")
+    implementation("org.jetbrains.exposed", "exposed-jdbc", "0.38.1")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.38.2")
+    // room
+    val roomVersion = "2.4.2"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    //kapt("androidx.room:room-compiler:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -6,12 +7,17 @@ pluginManagement {
         maven("https://maven.aliyun.com/repository/public")
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://plugins.gradle.org/m2/")
     }
     plugins{
+        kotlin("jvm") version extra["kotlin.version"] as String
         id("com.android.application") version  extra["android.tool.version"] as String
         id("org.jetbrains.kotlin.android") version  extra["kotlin.version"] as String
         id("com.android.library") version extra["android.tool.version"] as String
         id("org.jetbrains.kotlin.kapt") version extra["kotlin.version"] as String
+        kotlin("plugin.serialization") version extra["kotlin.version"] as String
+        id("org.jetbrains.kotlin.kapt") version extra["kotlin.version"] as String
+        id("com.google.devtools.ksp") version extra["ksp.version"] as String
     }
 }
 dependencyResolutionManagement {
@@ -23,6 +29,7 @@ dependencyResolutionManagement {
         maven("https://maven.aliyun.com/repository/public")
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
     }
 }
 rootProject.name = "jetpackComposeKtsTemplate"
